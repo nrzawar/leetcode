a035a2e8596770faa4e55a1ab7364a845ca29594
nrzawar <97452089+nrzawar@users.noreply.github.com>
2024-12-21 21:59:31 +0530

    Time: 1 ms (72.32%), Space: 44.8 MB (27.04%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.java
+++ 1651-shuffle-string/1651-shuffle-string.java
@@ -6,8 +6,8 @@ class Solution {
         for(int i=0; i<s.length();i++){
             c[indices[i]] = s.charAt(i);
         }
-        sb.append(c);
-        return sb.toString();
+        //sb.append(c);
+        return new String(c);
 
         
     }
