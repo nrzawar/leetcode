039b82b991f0739aeda9d517cf2fba998128caf4
nrzawar <97452089+nrzawar@users.noreply.github.com>
2024-12-21 22:25:54 +0530

    Time: 1 ms (65.61%), Space: 42.2 MB (6.4%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -1,8 +1,18 @@
 class Solution {
     public String interpret(String command) {
-        //command.replaceAll();
-        command = command.replace("()", "o");
-        command = command.replace("(al)", "al");
-        return command;
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='G'){
+                sb.append("G");
+            }
+            if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                sb.append("o");
+            }
+            if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                sb.append("al");
+            }
+        }
+        
+        return sb.toString();
     }
 }
